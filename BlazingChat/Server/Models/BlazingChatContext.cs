015b3cfd2c7d7621b2b39f26f05f6060686468fb
curious.drive <curious.drive@gmail.com>
2020-03-01 10:01:30 -0500

    Added EF Core to Server Project

---
+++ BlazingChat/Server/Models/BlazingChatContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlazingChat.Server.Models
+{
+    public partial class BlazingChatContext : DbContext
+    {
+        public BlazingChatContext()
+        {
+        }
+
+        public BlazingChatContext(DbContextOptions<BlazingChatContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacts> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source=C:\\Data\\CuriousDrive\\BlazingChat\\BlazingChat\\Server\\DB\\BlazingChat.db;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contacts>(entity =>
+            {
+                entity.HasKey(e => e.ContactId);
+
+                entity.ToTable("contacts");
+
+                entity.HasIndex(e => e.Email)
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Phone)
+                    .IsUnique();
+
+                entity.Property(e => e.ContactId)
+                    .HasColumnName("contact_id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("first_name");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("last_name");
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasColumnName("phone");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
