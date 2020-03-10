ca5174d30cf08f44ecbd0a7f4316c7e77f0a11a6
curious.drive <curious.drive@gmail.com>
2020-03-10 09:05:37 -0400

    Made CSS and JavaScript changes to make Blazing chat work

--- BlazingChat/Client/wwwroot/js/blazing.js
+++ BlazingChat/Client/wwwroot/js/blazing.js
@@ -3,3 +3,22 @@ function getMessageInput(){
     document.getElementById("txtMessageInput").value = '';
     return txtMessageInput;
 }
+
+function submitClick() {
+    
+    var data = document.getElementById("btn-input").value;    
+    
+    var chatLog = document.getElementById("chatLog");
+    chatLog.innerHTML += '<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div><div class="row msg_container base_receive"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div>';
+
+    //let's fix scroll here
+    var divMessageContainerBase = document.getElementById('divMessageContainerBase');
+    divMessageContainerBase.scrollTop = divMessageContainerBase.scrollHeight;
+
+  };
+  
+//   function clearInput() {
+//     $("#myForm :input").each(function() {
+//       $(this).val(''); //hide form values
+//     });
+//   }
