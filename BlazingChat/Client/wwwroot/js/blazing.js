6f42db097702b8735ed3a8b1edd5312bbc3469d8
curious.drive <curious.drive@gmail.com>
2020-03-11 08:51:21 -0400

    Making the chat window work

--- BlazingChat/Client/wwwroot/js/blazing.js
+++ BlazingChat/Client/wwwroot/js/blazing.js
@@ -4,12 +4,12 @@ function getMessageInput(){
     return txtMessageInput;
 }
 
-function submitClick() {
+function setScroll() {
     
-    var data = document.getElementById("btn-input").value;    
+    //var data = document.getElementById("btn-input").value;    
     
-    var chatLog = document.getElementById("chatLog");
-    chatLog.innerHTML += '<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div><div class="row msg_container base_receive"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div>';
+    //var chatLog = document.getElementById("chatLog");
+    //chatLog.innerHTML += '<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div><div class="row msg_container base_receive"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div>';
 
     //let's fix scroll here
     var divMessageContainerBase = document.getElementById('divMessageContainerBase');
