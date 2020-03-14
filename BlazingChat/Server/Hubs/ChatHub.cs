b79b99aa6003f50cc8dcb4bfb59f27be1e18e4a2
curious.drive <curious.drive@gmail.com>
2020-03-14 07:39:43 -0400

    Sending objects through SignalR connection

--- BlazingChat/Server/Hubs/ChatHub.cs
+++ BlazingChat/Server/Hubs/ChatHub.cs
@@ -8,9 +8,14 @@ namespace BlazingChat.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        // public async Task SendMessage(string user, string message)
+        // {
+        //     await Clients.All.SendAsync("ReceiveMessage", user, message);
+        // }
+
+        public async Task SendMessage(MessagePack messagePack)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", messagePack);
         }
     }
 }
---
+++ BlazingChat/Shared/Models/MessagePack.cs
@@ -0,0 +1,5 @@
+public class MessagePack {
+
+    public string UserName { get; set; }
+    public string Message { get; set; }
+}
