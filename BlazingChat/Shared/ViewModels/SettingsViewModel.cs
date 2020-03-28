62b950df754609a08ed6060229122e2ad6f68ad4
curious.drive <curious.drive@gmail.com>
2020-03-28 10:30:07 -0400

    Upgraded to 3.2 Preview 3

---
+++ BlazingChat/Shared/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazingChat.Shared.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public bool Notification { get; set; }
+        public string DarkTheme { get; set; }
+    }
+}
