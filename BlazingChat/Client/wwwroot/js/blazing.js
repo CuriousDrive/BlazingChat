681b09359d85ac4920378789e3cb64238ff36b3b
curious.drive <curious.drive@gmail.com>
2020-05-12 07:28:17 -0400

    Notification Changes

--- BlazingChat/Client/wwwroot/js/blazing.js
+++ BlazingChat/Client/wwwroot/js/blazing.js
@@ -1,24 +1,32 @@
-function getMessageInput(){
-    var txtMessageInput = document.getElementById("txtMessageInput").value;
-    document.getElementById("txtMessageInput").value = '';
-    return txtMessageInput;
+function getMessageInput() {
+  var txtMessageInput = document.getElementById("txtMessageInput").value;
+  document.getElementById("txtMessageInput").value = '';
+  return txtMessageInput;
 }
 
 function setScroll() {
-    
-    //var data = document.getElementById("btn-input").value;    
-    
-    //var chatLog = document.getElementById("chatLog");
-    //chatLog.innerHTML += '<div class="row msg_container base_sent"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div><div class="row msg_container base_receive"><div class="col-md-10 col-xs-10"><div class="messages msg_receive"><p>' + data + '</p></div></div></div>';
 
-    //let's fix scroll here
-    var divMessageContainerBase = document.getElementById('divMessageContainerBase');
-    divMessageContainerBase.scrollTop = divMessageContainerBase.scrollHeight;
+  //let's fix scroll here
+  var divMessageContainerBase = document.getElementById('divMessageContainerBase');
+  divMessageContainerBase.scrollTop = divMessageContainerBase.scrollHeight;
 
-  };
-  
-//   function clearInput() {
-//     $("#myForm :input").each(function() {
-//       $(this).val(''); //hide form values
-//     });
-//   }
+}
+
+function changeTitle() {
+
+  var currentTitle = document.title;
+  var splitCurrentTitle = currentTitle.split(' ');
+
+  if (splitCurrentTitle.length > 2) {
+
+    var currentCount = parseInt(splitCurrentTitle[0].replace('(','').replace(')',''));
+    var newCount = currentCount + 1;
+
+    document.title = '(' + newCount + ') Blazing Chat';
+
+  }
+  else {
+    document.title = '(1) Blazing Chat';
+  }
+
+}
