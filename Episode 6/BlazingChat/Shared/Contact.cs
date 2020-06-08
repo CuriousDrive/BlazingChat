a3e0be74736ee9e62c88b6f0d19474be21128c0d
curious.drive <curious.drive@gmail.com>
2020-06-08 16:28:45 -0400

    Getting ready for Episode 6

---
+++ Episode 6/BlazingChat/Shared/Contact.cs
@@ -0,0 +1,21 @@
+namespace BlazingChat.Shared
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(int contactId,string firstName, string lastName)
+        {
+            this.ContactId = contactId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
