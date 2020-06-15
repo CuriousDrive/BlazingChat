c51d617753cbe7b66e2a0f05f4a3be6fd0fa86b4
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-06-15 07:59:48 -0400

    Episode 7 - Initial Setup

--- Episode 7/BlazingChat/Client/Program.cs
+++ Episode 7/BlazingChat/Client/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Http;
-using BlazingChat.ViewModels;
 
 namespace BlazingChat.Client
 {
