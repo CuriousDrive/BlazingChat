dff04e1d43c5586896a4304aa781c03be326c6af
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-06-28 16:22:18 -0400

    Getting ready with Episode 8

---
+++ Episode 8/BlazingChat/Client/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,35 @@
+using BlazingChat.Shared.Models;
+
+namespace BlazingChat.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Message { get; set; }
+
+        public static implicit operator ProfileViewModel(User user)
+        {
+            return new ProfileViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                EmailAddress = user.EmailAddress,
+                UserId = user.UserId
+            };
+        }
+
+        public static implicit operator User(ProfileViewModel profileViewModel)
+        {
+            return new User
+            {
+                FirstName = profileViewModel.FirstName,
+                LastName = profileViewModel.LastName,
+                EmailAddress = profileViewModel.EmailAddress,
+                UserId = profileViewModel.UserId
+            };
+        }
+    }
+}
