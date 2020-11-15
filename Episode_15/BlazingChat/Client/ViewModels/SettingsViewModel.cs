b987258e891997e73ec5b8362183d9ba528617ff
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-11-15 08:29:03 -0500

    Getting ready for Google Authentication

---
+++ Episode_15/BlazingChat/Client/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using BlazingChat.Shared.Models;
+
+namespace BlazingChat.ViewModels
+{
+    public class SettingsViewModel : ISettingsViewModel
+    {
+        //properties
+        public long UserId { get; set; }
+        public bool Notifications { get; set; }
+        public bool DarkTheme { get; set; }
+        private HttpClient _httpClient;
+
+        //methods
+        public SettingsViewModel()
+        {
+        }
+        public SettingsViewModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task GetProfile()
+        {
+            User user = await _httpClient.GetFromJsonAsync<User>($"user/getprofile/{this.UserId}");
+            LoadCurrentObject(user);
+        }
+        public async Task Save()
+        {
+            await _httpClient.GetFromJsonAsync<User>($"user/updatetheme?userId={this.UserId}&value={this.DarkTheme.ToString()}");
+
+            await _httpClient.GetFromJsonAsync<User>($"user/updatenotifications?userId={this.UserId}&value={this.Notifications.ToString()}");
+        }
+        private void LoadCurrentObject(SettingsViewModel settingsViewModel)
+        {
+            this.DarkTheme = settingsViewModel.DarkTheme;
+            this.Notifications = settingsViewModel.Notifications;
+        }
+
+        //operators
+        public static implicit operator SettingsViewModel(User user)
+        {
+            return new SettingsViewModel
+            {
+                Notifications = (user.Notifications == null || (long)user.Notifications == 0) ? false : true,
+                DarkTheme = (user.DarkTheme == null || (long)user.DarkTheme == 0) ? false : true
+            };
+        }
+        public static implicit operator User(SettingsViewModel settingsViewModel)
+        {
+            return new User
+            {
+                Notifications = settingsViewModel.Notifications ? 1 : 0,
+                DarkTheme = settingsViewModel.DarkTheme ? 1 : 0
+            };
+        }
+    }
+}
---
+++ Episode_15/BlazingChat/Server/Startup.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using BlazingChat.Server.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace BlazingChat.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddEntityFrameworkSqlite().AddDbContext<BlazingChatContext>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }
+            ).AddCookie()
+            .AddTwitter(twitterOptions =>
+            {
+                twitterOptions.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
+                twitterOptions.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+		        twitterOptions.RetrieveUserDetails = true;
+            })
+            .AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
