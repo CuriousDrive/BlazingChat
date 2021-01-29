ebbc3287495b48de90d709cee9b6889f58bd1324
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-01-29 16:02:55 -0500

    Done with migrating from .net core 3.1 to .net 5

--- Episode_16/BlazingChat/Client/Program.cs
+++ Episode_16/BlazingChat/Client/Program.cs
@@ -18,12 +18,12 @@ namespace BlazingChat.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
 
-            builder.Services.AddTransient(sp => 
+            builder.Services.AddScoped(sp => 
                 new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddHttpClient<IProfileViewModel, ProfileViewModel>
