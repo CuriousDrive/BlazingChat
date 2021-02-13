77e19d48de8c8dbd370cb32deef1d6e262f9256d
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-02-13 10:24:32 -0500

    done with downloading profile pic

---
+++ Episode_19/BlazingChat/Client/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+function downloadFile(mimeType, base64String, fileName) {
+
+    var fileDataUrl = "data:" + mimeType + ";base64," + base64String;
+    fetch(fileDataUrl)
+        .then(response => response.blob())
+        .then(blob => {
+
+            //create a link
+            var link = window.document.createElement("a");
+            link.href = window.URL.createObjectURL(blob, { type: mimeType });
+            link.download = fileName;
+
+            //add, click and remove
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+    
+}
