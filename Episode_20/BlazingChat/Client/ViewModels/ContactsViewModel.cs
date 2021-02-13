0af405e52fe6549f052563eba371e92da3d83870
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-02-13 14:45:43 -0500

    Getting started with virtualize component

---
+++ Episode_20/BlazingChat/Client/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using BlazingChat.Shared.Models;
+
+namespace BlazingChat.ViewModels
+{
+    public class ContactsViewModel : IContactsViewModel
+    {
+        //properties
+        public List<Contact> Contacts { get; set; }
+        private HttpClient _httpClient;
+
+        //methods
+        public ContactsViewModel()
+        {
+        }
+        public ContactsViewModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task GetContacts()
+        {
+            List<User> users = await _httpClient.GetFromJsonAsync<List<User>>("user/getcontacts");
+            LoadCurrentObject(users);
+        }
+
+        private void LoadCurrentObject(List<User> users)
+        {
+            this.Contacts = new List<Contact>();
+            foreach (User user in users)
+            {
+                this.Contacts.Add(user);
+            }
+        }
+
+
+    }
+}
