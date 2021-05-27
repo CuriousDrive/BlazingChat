f63e392594e8fec3b0ed1b6dfdf70b9d5c94a869
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-05-27 16:55:46 -0400

    Done with Episode 31

--- src/Client/wwwroot/js/site.js
+++ src/Client/wwwroot/js/site.js
@@ -1,4 +1,20 @@
 
+export function setTheme(themeName) {
+    
+    //add a new css link
+    let newLink = document.createElement("link");
+    newLink.setAttribute("id", "theme");
+    newLink.setAttribute("rel", "stylesheet");
+    newLink.setAttribute("type", "text/css");
+    newLink.setAttribute("href", `css/app-${themeName}.css`);
+
+    //remove the theme from the head tag
+    let head = document.getElementsByTagName("head")[0];
+    head.querySelector("#theme").remove();
+
+    head.appendChild(newLink);
+}
+
 export function downloadFile(mimeType, base64String, fileName) {
 
     var fileDataUrl = "data:" + mimeType + ";base64," + base64String;
