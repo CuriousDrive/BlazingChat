18ebc9674e0ae0a493cac41b422ffe880cd489b3
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-05-29 10:08:43 -0400

    added some comments

--- src/Client/wwwroot/js/site.js
+++ src/Client/wwwroot/js/site.js
@@ -1,17 +1,15 @@
 
-export function setTheme(themeName) {
-    
+export function setTheme(themeName) {    
     //add a new css link
     let newLink = document.createElement("link");
     newLink.setAttribute("id", "theme");
     newLink.setAttribute("rel", "stylesheet");
     newLink.setAttribute("type", "text/css");
     newLink.setAttribute("href", `css/app-${themeName}.css`);
-
     //remove the theme from the head tag
     let head = document.getElementsByTagName("head")[0];
     head.querySelector("#theme").remove();
-
+    //adding newLink in the head
     head.appendChild(newLink);
 }
 
