190bf4b7a2df0660cd6a16ac56ef1fde19797b10
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-08-09 16:06:51 -0400

    Reverting Changes - CI/CD Process

--- src/WebAPI/Controllers/WeatherForecastController.cs
+++ src/WebAPI/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace BlazingChat.WebAPI.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
