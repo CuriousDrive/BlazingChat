36d7070e4ed0022cabbf8ba7500d3b968cb08766
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-11-26 03:30:54 -0500

    Done with program.cs changes

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazingChat.Client;
 using BlazingChat.Client.Handlers;
 using BlazingChat.Client.Logging;
 using BlazingChat.ViewModels;
@@ -12,63 +13,54 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace BlazingChat.Client
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+#region ConfigureServices
+
+string baseAddress = builder.Configuration["BaseAddress"];
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+builder.Services.ConfigureAll<HttpClientFactoryOptions>(options =>
+    options.HttpMessageHandlerBuilderActions.Add(handlerBuilder =>
+        handlerBuilder.AdditionalHandlers.Add(
+            handlerBuilder.Services.GetRequiredService<CustomAuthorizationHandler>())));
+
+//transactional named http clients
+builder.Services.AddHttpClient<IProfileViewModel, ProfileViewModel>
+    ("ProfileViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
+
+builder.Services.AddHttpClient<IContactsViewModel, ContactsViewModel>
+    ("ContactsViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
+
+builder.Services.AddHttpClient<ISettingsViewModel, SettingsViewModel>
+    ("SettingsViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
+
+builder.Services.AddHttpClient<IAssignRolesViewModel, AssignRolesViewModel>
+    ("AssignRolesViewModel", client => client.BaseAddress = new Uri(baseAddress));
+
+//authentication http clients
+builder.Services.AddHttpClient<ILoginViewModel, LoginViewModel>
+    ("LoginViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
+
+builder.Services.AddHttpClient<IRegisterViewModel, RegisterViewModel>
+    ("RegisterViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddTransient<CustomAuthorizationHandler>();
+builder.Services.AddLogging(logging =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    var httpClient = builder.Services.BuildServiceProvider().GetRequiredService<HttpClient>();
+    var authenticationStateProvider = builder.Services.BuildServiceProvider().GetRequiredService<AuthenticationStateProvider>();
+    logging.SetMinimumLevel(LogLevel.Error);
+    logging.AddProvider(new ApplicationLoggerProvider(httpClient, authenticationStateProvider));
+});
+builder.Services.AddBlazoredToast();
 
-            #region ConfigureServices
+#endregion
 
-            string baseAddress = builder.Configuration["BaseAddress"];
-
-            builder.Services.AddOptions();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-
-            builder.Services.ConfigureAll<HttpClientFactoryOptions>(options =>
-                options.HttpMessageHandlerBuilderActions.Add(handlerBuilder =>
-                    handlerBuilder.AdditionalHandlers.Add(
-                        handlerBuilder.Services.GetRequiredService<CustomAuthorizationHandler>())));
-
-            //transactional named http clients
-            builder.Services.AddHttpClient<IProfileViewModel, ProfileViewModel>
-                ("ProfileViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
-
-            builder.Services.AddHttpClient<IContactsViewModel, ContactsViewModel>
-                ("ContactsViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
-
-            builder.Services.AddHttpClient<ISettingsViewModel, SettingsViewModel>
-                ("SettingsViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
-
-            builder.Services.AddHttpClient<IAssignRolesViewModel, AssignRolesViewModel>
-                ("AssignRolesViewModel", client => client.BaseAddress = new Uri(baseAddress));
-
-            //authentication http clients
-            builder.Services.AddHttpClient<ILoginViewModel, LoginViewModel>
-                ("LoginViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
-
-            builder.Services.AddHttpClient<IRegisterViewModel, RegisterViewModel>
-                ("RegisterViewModelClient", client => client.BaseAddress = new Uri(baseAddress));
-
-            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-            builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddTransient<CustomAuthorizationHandler>();
-            builder.Services.AddLogging(logging =>
-            {
-                var httpClient = builder.Services.BuildServiceProvider().GetRequiredService<HttpClient>();
-                var authenticationStateProvider = builder.Services.BuildServiceProvider().GetRequiredService<AuthenticationStateProvider>();
-                logging.SetMinimumLevel(LogLevel.Error);
-                logging.AddProvider(new ApplicationLoggerProvider(httpClient, authenticationStateProvider));
-            });
-            builder.Services.AddBlazoredToast();
-
-            #endregion
-            
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
