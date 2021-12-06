a967d42981ee6ce69443fcd599a413c1af25e9dc
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-12-05 21:45:31 -0500

    Done with service extension methods demo

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,16 +1,7 @@
 using BlazingChat.Client;
-using BlazingChat.Client.Handlers;
-using BlazingChat.Client.Logging;
-using BlazingChat.ViewModels;
-using Blazored.LocalStorage;
-using Blazored.Toast;
-using Microsoft.AspNetCore.Components.Authorization;
+using BlazingChat.Shared.Extensions;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Http;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -19,44 +10,7 @@ builder.RootComponents.Add<App>("#app");
 
 var baseAddress = builder.Configuration["BaseAddress"] ??
                      throw new NullReferenceException("BaseAddress is missing in configuration");
-
-// authetication & authorization
-builder.Services.AddOptions();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-builder.Services.AddTransient<CustomAuthorizationHandler>();
-
-// configuring http clients
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
-builder.Services.ConfigureAll<HttpClientFactoryOptions>(options =>
-    options.HttpMessageHandlerBuilderActions.Add(handlerBuilder =>
-        handlerBuilder.AdditionalHandlers.Add(
-            handlerBuilder.Services.GetRequiredService<CustomAuthorizationHandler>())));
-
-var clientConfigurator = void (HttpClient client) => client.BaseAddress = new Uri(baseAddress);
-
-// transactional named http clients
-builder.Services.AddHttpClient<IProfileViewModel, ProfileViewModel>("ProfileViewModelClient", clientConfigurator);
-builder.Services.AddHttpClient<IContactsViewModel, ContactsViewModel>("ContactsViewModelClient", clientConfigurator);
-builder.Services.AddHttpClient<ISettingsViewModel, SettingsViewModel>("SettingsViewModelClient", clientConfigurator);
-builder.Services.AddHttpClient<IAssignRolesViewModel, AssignRolesViewModel>("AssignRolesViewModel", clientConfigurator);
-
-// authentication http clients
-builder.Services.AddHttpClient<ILoginViewModel, LoginViewModel>("LoginViewModelClient", clientConfigurator);
-builder.Services.AddHttpClient<IRegisterViewModel, RegisterViewModel>("RegisterViewModelClient", clientConfigurator);
-
-// logging
-builder.Services.AddLogging(logging =>
-{
-    var httpClient = builder.Services.BuildServiceProvider().GetRequiredService<HttpClient>();
-    var authStateProvider = builder.Services.BuildServiceProvider().GetRequiredService<AuthenticationStateProvider>();
-    logging.SetMinimumLevel(LogLevel.Error);
-    logging.AddProvider(new ApplicationLoggerProvider(httpClient, authStateProvider));
-});
-
-// blazored services
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddBlazoredToast();
+builder.Services.AddBlazingChat(baseAddress);
 
 #endregion
 
---
+++ src/Shared/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,61 @@
+﻿using BlazingChat.Client;
+using BlazingChat.Client.Handlers;
+using BlazingChat.Client.Logging;
+using BlazingChat.ViewModels;
+using Blazored.LocalStorage;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+
+namespace BlazingChat.Shared.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddBlazingChat(this IServiceCollection services, string baseAddress)
+        {
+            // blazored services
+            services.AddBlazoredLocalStorage();
+            services.AddBlazoredToast();
+
+            // authetication & authorization
+            services.AddOptions();
+            services.AddAuthorizationCore();
+            services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            services.AddTransient<CustomAuthorizationHandler>();
+
+            // configuring http clients
+            services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+            services.ConfigureAll<HttpClientFactoryOptions>(options =>
+                options.HttpMessageHandlerBuilderActions.Add(handlerBuilder =>
+                    handlerBuilder.AdditionalHandlers.Add(
+                        handlerBuilder.Services.GetRequiredService<CustomAuthorizationHandler>())));
+
+            var clientConfigurator = void (HttpClient client) => client.BaseAddress = new Uri(baseAddress);
+
+            // transactional named http clients
+            services.AddHttpClient<IProfileViewModel, ProfileViewModel>("ProfileViewModelClient", clientConfigurator);
+            services.AddHttpClient<IContactsViewModel, ContactsViewModel>("ContactsViewModelClient", clientConfigurator);
+            services.AddHttpClient<ISettingsViewModel, SettingsViewModel>("SettingsViewModelClient", clientConfigurator);
+            services.AddHttpClient<IAssignRolesViewModel, AssignRolesViewModel>("AssignRolesViewModel", clientConfigurator);
+
+            // authentication http clients
+            services.AddHttpClient<ILoginViewModel, LoginViewModel>("LoginViewModelClient", clientConfigurator);
+            services.AddHttpClient<IRegisterViewModel, RegisterViewModel>("RegisterViewModelClient", clientConfigurator);
+
+            // logging
+            services.AddLogging(logging =>
+            {
+                var httpClient = services.BuildServiceProvider().GetRequiredService<HttpClient>();
+                var authStateProvider = services.BuildServiceProvider().GetRequiredService<AuthenticationStateProvider>();
+                logging.SetMinimumLevel(LogLevel.Error);
+                logging.AddProvider(new ApplicationLoggerProvider(httpClient, authStateProvider));
+            });
+
+            return services;
+        }
+    }
+}
