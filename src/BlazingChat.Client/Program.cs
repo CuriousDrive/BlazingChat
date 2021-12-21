42b57badf3985f2e7f1acde59aef5a1e1b3162ef
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-12-21 09:57:14 -0500

    Added code for Env managment

--- src/BlazingChat.Client/Program.cs
+++ src/BlazingChat.Client/Program.cs
@@ -1,23 +1,29 @@
-using System;
 using BlazingChat.Client;
 using BlazingChat.Shared.Extensions;
 using BlazingChat.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 #region ConfigureServices
 
-var applicationSettingsSection = builder.Configuration.GetSection("ApplicationSettings");
+// setting client host environment 
+builder.Services.AddSingleton<IHostingEnvironment>(
+    new HostingEnvironment() { EnvironmentName = builder.HostEnvironment.Environment });
 
+// adding client app settings 
+var applicationSettingsSection = builder.Configuration.GetSection("ApplicationSettings");
 builder.Services.Configure<ApplicationSettings>(options =>
 {
     applicationSettingsSection.Bind(options);
 });
 
+// adding application services
 builder.Services.AddBlazingChat(applicationSettingsSection.Get<ApplicationSettings>());
 
 #endregion
