70f55364df3123f3120ff96b102619c6e275dd5b
Curious Drive <CuriousDrive@users.noreply.github.com>
2022-02-01 14:32:08 -0500

    testing actions

--- src/BlazingChat.Components/ExampleJsInterop.cs
+++ src/BlazingChat.Components/ExampleJsInterop.cs
@@ -5,7 +5,7 @@ namespace BlazingChat.Components
     // This class provides an example of how JavaScript functionality can be wrapped
     // in a .NET class for easy consumption. The associated JavaScript module is
     // loaded on demand when first needed.
-    //
+    // testing
     // This class can be registered as scoped DI service and then injected into Blazor
     // components for use.
 
